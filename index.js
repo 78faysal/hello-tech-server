3af020d37d58a7ea4334cdfb7f5b63ef60781b02
mdfaysal <faysal7830@gmail.com>
2023-05-03 22:58:34 +0600

    data structure added

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+
+const coursesData = require('./data/coursesData.json');
+
 app.get('/', (req, res) => {
-    res.send('Hello from hello tech server')
+    res.send('Hello from hello tech server', coursesData)
 })
 
+app.get('/coursesData', (req, res) => {
+    res.send(coursesData)
+});
+
+
 app.listen(port, () => {
-    console.log(`listening on port ${port}`)
+    console.log(`listening on port ${port}, ${coursesData}`)
 })
