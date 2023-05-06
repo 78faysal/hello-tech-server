5c696ceeb95b785a94c47fbd3b378772a2ebf1af
mdfaysal <faysal7830@gmail.com>
2023-05-06 23:32:13 +0600

    all data added

--- index.js
+++ index.js
@@ -2,18 +2,16 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
-
 const coursesData = require('./data/coursesData.json');
 
 app.get('/', (req, res) => {
-    res.status(200).send('Hello from hello tech server', coursesData)
-})
-
-app.get('/coursesData', (req, res) => {
-    res.send(coursesData)
+  res.status(200).send('hello from hello tech');
 });
 
+app.get('/coursesData', (req, res) => {
+  res.send(coursesData);
+});
 
 app.listen(port, () => {
-    console.log(`listening on port ${port}`)
-})
+  console.log(`listening on port ${port}`);
+});
