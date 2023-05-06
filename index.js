eae3ef66a85206a3eef889b0e3dcc2ee68566249
mdfaysal <faysal7830@gmail.com>
2023-05-06 23:04:39 +0600

    image url added

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000;
 const coursesData = require('./data/coursesData.json');
 
 app.get('/', (req, res) => {
-    res.send('Hello from hello tech server', coursesData)
+    res.status(200).send('Hello from hello tech server', coursesData)
 })
 
 app.get('/coursesData', (req, res) => {
@@ -15,5 +15,5 @@ app.get('/coursesData', (req, res) => {
 
 
 app.listen(port, () => {
-    console.log(`listening on port ${port}, ${coursesData}`)
+    console.log(`listening on port ${port}`)
 })
